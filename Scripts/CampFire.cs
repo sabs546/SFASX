dced98806ef158d5caf00a1192196d4709c1ec18
sabs546 <55698170+sabs546@users.noreply.github.com>
2020-01-26 23:07:04 +0000

    Add files via upload

---
+++ Scripts/CampFire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampFire : InventorySlot
+{
+    public override void SetValues(ref InventorySlot item)
+    {
+        item.itemName   = " campfire";
+        item.placeable  = true;
+        item.itemCount  = 1;
+        item.itemType   = 13;
+        item.placeIndex = 1;
+        item.iconIndex  = 6;
+
+        item.ingredients = new int[2];
+        item.ingCount = new int[2];
+        item.ingredients[0] = 1;
+        item.ingredients[1] = 3;
+        item.ingCount[0] = 8;
+        item.ingCount[1] = 13;
+    }
+}
